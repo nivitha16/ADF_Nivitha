0c22b7bb0a86e8db3a54d47190547124e6bbbed8
nivitha16 <hemsnivi1606@gmail.com>
2021-06-30 23:34:38 +0530

    version1

--- TestPro/userinformation/apps.py
+++
@@ -1,8 +0,0 @@
-"""apps.py"""
-from django.apps import AppConfig
-
-
-class UserinformationConfig(AppConfig):
-    """UserinformationConfig"""
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'userinformation'
--- TestPro/userinformation/urls.py
+++
@@ -1,9 +0,0 @@
-"""userinformation-urls"""
-from django.urls import path
-from .import views
-
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('userinfosubmission', views.userinfosubmission, name='userinfosubmission')
-]
